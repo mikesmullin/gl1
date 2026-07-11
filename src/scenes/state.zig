79571551cb09c60ea0177809078a10b4697acfdd
me <user@home.local>
2026-07-11 11:36:46 -0600

    Default Notes textarea to a lorem ipsum paragraph

--- src/scenes/state.zig
+++ src/scenes/state.zig
@@ -83,7 +83,9 @@ pub const State = struct {
         const en = "Hero";
         @memcpy(self.entity_name[0..en.len], en);
         self.entity_name_len = en.len;
-        const note = "Notes…";
+        const note =
+            \\Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        ;
         @memcpy(self.notes[0..note.len], note);
         self.notes_len = note.len;
     }
